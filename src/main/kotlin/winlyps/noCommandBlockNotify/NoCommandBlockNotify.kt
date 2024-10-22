606dfabbe52228cfef1bf122d4ccb6ca8876583c
Winlypse <nts3@o2.pl>
2024-10-22 14:12:26 +0200

    Changes!

--- src/main/kotlin/winlyps/noCommandBlockNotify/NoCommandBlockNotify.kt
+++ src/main/kotlin/winlyps/noCommandBlockNotify/NoCommandBlockNotify.kt
@@ -6,9 +6,14 @@ class NoCommandBlockNotify : JavaPlugin() {
 
     override fun onEnable() {
         // Plugin startup logic
+        server.worlds.forEach { world ->
+            world.setGameRuleValue("commandBlockOutput", "false")
+        }
+        logger.info("NoCommandBlockNotify plugin has been enabled.")
     }
 
     override fun onDisable() {
         // Plugin shutdown logic
+        logger.info("NoCommandBlockNotify plugin has been disabled.")
     }
 }
