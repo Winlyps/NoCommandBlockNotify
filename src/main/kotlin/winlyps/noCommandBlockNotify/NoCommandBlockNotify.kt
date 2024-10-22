b3d344aaf64de8c69fe940becdfc4615d30826b8
Winlypse <nts3@o2.pl>
2024-10-22 14:12:15 +0200

    Initial commit

---
+++ src/main/kotlin/winlyps/noCommandBlockNotify/NoCommandBlockNotify.kt
@@ -0,0 +1,14 @@
+package winlyps.noCommandBlockNotify
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class NoCommandBlockNotify : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
